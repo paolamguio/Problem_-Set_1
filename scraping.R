e77c0cd845a47689c40a434ff7c26ec4defd2296
paolamguio <ap.morales1967@uniandes.edu.co>
2022-06-18 14:30:14 -0500

    Ajustes
    Ajustes al llamado de las tablas del GEIH

--- scraping.R
+++ scraping.R
@@ -1,14 +1,38 @@
+########################################
+############### DATOS ##################
+############# PROBLEM_SET_1 ############
+
+
+# Limpieza de espacio
 rm(list = ls())
 
+# Llamado librería
 require(pacman)
 
-p_load(tidyverse, # contiene las librerías ggplot, dplyr...
-       rvest)# web-scraping
+p_load(tidyverse,
+       rvest)
+# scrape de datos como tabla desde sitio el web:
+url <- "https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_"
 
-url <- "https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_1.html"
+# Almacenamiento de los datos en tablas
 
-tabla <- url %>% read_html() %>% html_table()
-
+tablas = list()
 for (i in 1:10) {
-  url[i] <- paste("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_", i, ".html", sep = "")
-}
+  url_i = paste0(url,i,".html")
+  tablas[[i]] <- url_i %>% read_html() %>% html_table()
+}  
+
+# Revisión de tablas 
+tablas[[1]]
+tablas[[5]]  
+
+# Renombre primera columna 
+
+library(dplyr)
+data(package="datasets")
+
+colnames(tablas)[2] <- "item"
+# Revisión de tablas
+tablas[[2]]
+
+

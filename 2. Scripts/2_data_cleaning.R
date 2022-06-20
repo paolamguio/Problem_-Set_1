ea63eff07cc57359277b8f293f78cc69719ecb92
paolamguio <ap.morales1967@uniandes.edu.co>
2022-06-19 22:48:05 -0500

    Labels variables adicionales
    Se incluye archivo en Stores

--- 2. Scripts/2_data_cleaning.R
+++ 2. Scripts/2_data_cleaning.R
@@ -22,6 +22,8 @@ p_load(
   skimr
 )
 
+
+## Data cleaning
 # Se importa base de datos obtenida del scraping
 df <- import("data.rds")
 
@@ -33,12 +35,24 @@ skim(df) %>% head()
 # estadísticas descriptivas de las variables
 summary(df)
 
-table(df$dominio)
+# Verificación información de variables
 
-table(df$dsi)
+table(df$dominio) #Verificamos que todos los datos corresponden a Bogotá, departamento Cundinamarca
 
-table(df$ocu)
+table(df$dsi) #la variable dsi indica en cero las personas empleadas
+
+table(df$ocu) #la variable ocu indica en uno las personas ocupadas
+
+table(df$clase) #todos los individuos estan en la zona urbana
+
+table(df$formal) #9676 personas hacen parte del sistema de seguridad social, trabajo formal
+
+table(df$relab)
 
 summary(df$ingtot)
 
-df <- df %>% subset(age > 18 & ocu == 1)
+summary(df$y_total_m) # Andres, esta es la otra variable que teníamos pata análizar ocmo ingreso total
+
+# filtro de personas mayores de 18 años y ocupados
+df <- df %>% subset(age > 18 & ocu == 1) 
+

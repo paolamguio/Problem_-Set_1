795c0b4cfe57a71530f8fce6cf4c362caba9a558
felipemartinez0812 <af.martinezv12@uniandes.edu.co>
2022-06-22 14:27:14 -0500

    Se actualiza script y se incluye estimación de earnings gap

--- 2. Scripts/4_earnings_gap.R
+++ 2. Scripts/4_earnings_gap.R
@@ -34,8 +34,31 @@ summary(df$ingtot)
 
 df<- df %>% mutate(logingtot=log(ingtot))
 
+df <- df %>% mutate(age2 = age^2)
+
 ## Estimación función brecha de ingresos por género
 
 reg <- lm(logingtot~female, df)
 
 stargazer(reg, type = "text")
+
+reg2 <- lm(logingtot~female + age + age2 + age:female, df)
+
+stargazer(reg, reg2, type = "text")
+
+coef <- reg2$coef
+
+b1 <- coef[2]
+b2 <- coef[3]
+b3 <- coef[4]
+b4 <- coef[5]
+
+age_bar <- mean(df$age)
+
+elast <- b1 + b4*age_bar
+
+df <- df %>% ungroup() %>% mutate(y_hat = predict(reg2))
+
+ggplot(data = df , 
+       mapping = aes(x = age , y = y_hat , group=as.factor(female) , color=as.factor(female))) +
+  geom_point()

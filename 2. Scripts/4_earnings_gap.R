80786c7b4a7a7bd8fd284307c6174b817263b22e
felipemartinez0812 <af.martinezv12@uniandes.edu.co>
2022-06-23 00:28:18 -0500

    Se actualiza script

--- 2. Scripts/4_earnings_gap.R
+++ 2. Scripts/4_earnings_gap.R
@@ -21,7 +21,8 @@ p_load(
   rio,
   skimr,
   pastecs,
-  stargazer
+  stargazer,
+  boot
 )
 
 library(datasets)
@@ -30,9 +31,9 @@ library(data.table)
 # Llamado base de datos generada en 2_data_cleaning.R
 df <- import("df.rds")
 
-summary(df$ingtot)
+summary(df$y_total_m)
 
-df<- df %>% mutate(logingtot=log(ingtot))
+df<- df %>% mutate(logingtot=log(y_total_m))
 
 df <- df %>% mutate(age2 = age^2)
 
@@ -62,3 +63,41 @@ df <- df %>% ungroup() %>% mutate(y_hat = predict(reg2))
 ggplot(data = df , 
        mapping = aes(x = age , y = y_hat , group=as.factor(female) , color=as.factor(female))) +
   geom_point()
+
+peak_age_female <- (-b2-b4)/(2*b3)
+peak_age_male <- (-b2)/(2*b3)
+
+eta_mod.fn <- function(data, index, female_bar){
+  f <- lm(logingtot~female + age + age2 + age:female, data, subset = index)
+  
+  coef <- f$coef
+  
+  b1 <- coef[2]
+  b2 <- coef[3]
+  b3 <- coef[4]
+  b4 <- coef[5]
+  
+  peak_age <- (-b2-b4*female_bar)/(2*b3)
+  return(peak_age)
+}
+
+eta_mod.fn(df, 1:14631, 1)
+eta_mod.fn(df, 1:14631, 0)
+
+set.seed(1)
+
+boot_female <- boot(data = df, eta_mod.fn, female_bar = 1, R = 5000)
+
+boot_female
+
+boot_male <- boot(data = df, eta_mod.fn, female_bar = 0, R = 5000)
+
+boot_male
+
+CI_female <- c(peak_age_female - 1.96*0.4722849, peak_age_female + 1.96*0.4722849)
+
+CI_female
+
+CI_male <- c(peak_age_male - 1.96*0.3856051, peak_age_male + 1.96*0.3856051)
+
+CI_male
